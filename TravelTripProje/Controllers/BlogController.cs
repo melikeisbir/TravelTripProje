7b7d89aa27b5d8d0a51b7a3ba877269011185246
Melike İsbir <melikeisbr@gmail.com>
2023-08-20 22:31:15 +0300

    update

--- TravelTripProje/Controllers/BlogController.cs
+++ TravelTripProje/Controllers/BlogController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
 
 namespace TravelTripProje.Controllers
 {
     public class BlogController : Controller
     {
         // GET: Blog
+        Context c = new Context();
         public ActionResult Index()
+        {
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
+        }
+        public ActionResult BlogDetay(int id)
         {
             return View();
         }
