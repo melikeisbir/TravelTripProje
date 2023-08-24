d5295b85f4149e4cdb6cf56a587d0526bb1cb944
Melike İsbir <melikeisbr@gmail.com>
2023-08-24 23:23:26 +0300

    admin comment page, delete comment

--- TravelTripProje/Controllers/AdminController.cs
+++ TravelTripProje/Controllers/AdminController.cs
@@ -52,5 +52,31 @@ namespace TravelTripProje.Controllers
             c.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+        public ActionResult YorumSil(int id)
+        {
+            var b = c.Yorumlars.Find(id); 
+            c.Yorumlars.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+        public ActionResult YorumGetir(int id)
+        {
+            var yr = c.Yorumlars.Find(id);
+            return View("YorumGetir", yr);
+        }
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+            var yrm = c.Yorumlars.Find(y.ID);
+            yrm.KullaniciAdi = y.KullaniciAdi;
+            yrm.Mail = y.Mail;
+            yrm.Yorum = y.Yorum;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
